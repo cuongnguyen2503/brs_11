d57dd2dec804a15af6dc53be0cf094989aff6cdc
VuHongHiep <vu.hong.hiep@framgia.com>
2015-03-31 09:34:58 +0700

    filter_by_category_title_author

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,7 @@
 class Book < ActiveRecord::Base
   belongs_to :category
+
+  scope :at_category, -> category {where(category: category) if category}
+  scope :title_like, -> title {where("title like ?", "%#{title}%")}
+  scope :author_like, -> author {where("author like ?", "%#{author}%")}
 end
